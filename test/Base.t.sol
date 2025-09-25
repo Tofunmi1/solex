880236e271fd8873d8e8117588a4856f05fe2514
Adediran Tofunmi <folakemiadediran14@gmail.com>
2025-09-25 05:41:07 +0100

    test template ci check

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -1,4 +1,14 @@
 //// SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-contract Base {}
+import {Test} from "lib/forge-std/src/Test.sol";
+
+contract Base is Test {
+    function setUp() public {}
+
+    function test_Name_ShouldDoSomething_WhenSomethingHappens() public {
+        //setUp
+        //execution
+        //assert
+    }
+}
